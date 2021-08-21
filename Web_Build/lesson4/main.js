50946e212690c9311f8e2720eb32b286beab10f4
CorkyCroc <86145789+CorkyCroc@users.noreply.github.com>
2021-08-21 14:33:05 +0700

    Add hw

---
+++ Web_Build/lesson4/main.js
@@ -0,0 +1,7 @@
+console.log("HELLO WORLD");
+console.log("1 + 2");
+let a = 8
+let b = 12
+console.log("ket qua cua a + b la", a + b, "hello");
+console.log("Ket qua cua a + b la " + (a+b) + "hello");
+console.log(`ket qua cua ${a} + ${b} la ${a+b} hello`);
