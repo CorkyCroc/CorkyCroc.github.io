e72827fae87909747821e35c93194d4866ca12ff
CorkyCroc <86145789+CorkyCroc@users.noreply.github.com>
2021-09-22 22:50:15 +0700

    final proj

--- Web_Build/lesson8/index.js
+++ Web_Build/lesson8/index.js
@@ -1,11 +1,18 @@
 let formin = document.getElementById('input')
-formin.onsubmit = async function Getdata(city) {
+
+formin.onsubmit = function(e) {
+  e.preventDefault();
+  let key = formin.cityName.value
+  Getdata(key)
+}
+
+async function Getdata(city) {
   let drawdata = await fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=7674da634845e7c2d8c53ab0a48b8e29`
   );
   let data = await drawdata.json();
 
-  let key = formin.cityName.value
+
   let domWeather = document.querySelector(".container");
   let html = `<div class="card">
   <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="">
@@ -16,4 +23,3 @@ formin.onsubmit = async function Getdata(city) {
 
     domWeather.innerHTML = html;
   }
-Getdata(`${key}`);
