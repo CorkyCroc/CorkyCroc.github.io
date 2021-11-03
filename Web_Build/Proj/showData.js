62c080c26e2a07bfe503437fcfd31cd21ac127c4
CorkyCroc <86145789+CorkyCroc@users.noreply.github.com>
2021-11-03 08:03:33 +0700

    fix

--- Web_Build/Proj/showData.js
+++ Web_Build/Proj/showData.js
@@ -17,7 +17,7 @@ let renderFullData = (data)=>{
     for(let i=0; i< data.length; i++){
       let html = `<div class="card">
       <h2>${data[i].name}</h2>
-      <p><i class="fad fa-lungs-virus"></i>Cases: ${data[i].cases}</p>
+      <p><i class="fas fa-virus"></i>Total Cases: ${data[i].cases}</p>
       <p><i class="fas fa-ambulance"></i>Cases Today: ${data[i].casesToday}</p>
       <p><i class="fas fa-skull"></i>Death: ${data[i].death}</p>
       <p><i class="fad fa-procedures"></i>Treating: ${data[i].treating}</p>
