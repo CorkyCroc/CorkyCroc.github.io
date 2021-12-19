b17568edc8910731f22419cd76789493e1f90ac1
CorkyCroc <86145789+CorkyCroc@users.noreply.github.com>
2021-12-19 16:04:38 +0700

    hw

---
+++ Web_Build_JSA/lesson4_hw/index.js
@@ -0,0 +1,51 @@
+
+
+// var number= [4,,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+
+// function findEven(number){
+
+//     // var number= [4,,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+
+//     if(number % 2 == 0){
+//         console.log("Even")
+//     } else {
+//         console.log("Odd")
+//     }
+// };
+
+
+
+
+
+// var div = document.getElementById("background");
+// div.onclick = function () {
+//     div.style.background = "red";
+// };
+
+
+
+
+// for(i=0;i<5;i++) {
+//     for(j=0;j<=i;j++) {
+//         document.writeln(" 1 ")
+//     }
+//     document.writeln("<br/>")
+// }
+
+
+
+
+
+
+// function Dong_ho() {
+//     var gio = document.getElementById("gio");
+//     var phut = document.getElementById("phut");
+//     var giay = document.getElementById("giay");
+//     var Gio_hien_tai = new Date().getHours();
+//     var Phut_hien_tai = new Date().getMinutes();
+//     var Giay_hien_tai = new Date().getSeconds();
+//     gio.innerHTML = Gio_hien_tai;
+//     phut.innerHTML = Phut_hien_tai;
+//     giay.innerHTML = Giay_hien_tai;
+// }
+// var Dem_gio = setInterval(Dong_ho, 1000);
