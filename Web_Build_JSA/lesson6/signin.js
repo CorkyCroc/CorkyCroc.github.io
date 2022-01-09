78ed680240a55f0e5a43794627ae95c3f0974165
CorkyCroc <86145789+CorkyCroc@users.noreply.github.com>
2022-01-09 19:33:29 +0700

    update

--- Web_Build_JSA/lesson6/signin.js
+++ Web_Build_JSA/lesson6/signin.js
@@ -1,7 +1,43 @@
-let signInForm = document.getElementById("signInForm")
-signInForm.onsubmit = (e) =>{
-   e.preventDefault()
-    let email = signInForm.email.value
-    let password = signInForm.password.value
-    console.log(email, password);
+let formSignIn = document.getElementById("formSignIn");
+
+let setTextErr = (query, content) => {
+  document.querySelector(query).innerHTML = content;
+};
+
+let users = [];
+let userslist = JSON.parse(localStorage.getItem("users")) ;
+if (userslist) {
+  users = userslist;
 }
+
+formSignIn.onsubmit = (e) => {
+  e.preventDefault();
+  let email = formSignIn.email.value;
+  let password = formSignIn.password.value;
+
+  let valid = true;
+
+  if (!email) {
+    setTextErr("#emailErr", "Email is required");
+    valid = false;
+  }
+  if (!password) {
+    setTextErr("#passwordErr", "Password is required");
+    valid = false;
+  }
+
+    if (valid) {
+      let success = false;
+      for (let i = 0; i < users.length; i++) {
+        if (users[i].email == email && users[i].password == password) {
+          open("home.html", "_self");
+          success = true;
+          break;
+        }
+      }
+      if (success == false) {
+        alert("Sign in failed");
+      }
+    }
+
+};
--- Web_Build_JSA/lesson6/signup.js
+++ Web_Build_JSA/lesson6/signup.js
@@ -1,51 +1,57 @@
-let signInForm = document.getElementById("signInForm")
+let signUpform = document.getElementById("signUpForm")
 
-let usersFromLocal = JSON.parse(localStorage.getItem("users"))
-
-let users= []
+let users = []
+let usersFromLocal = JSON.parse(localStorage.getItem("users"))  
+if(usersFromLocal){
+    users = usersFromLocal
+}
 
 signUpform.onsubmit = (e) =>{
     e.preventDefault()
-     setTextErr("#emailErr", " ")
-     setTextErr("#passwordErr", " ")
-     setTextErr("#confirmPasswordErr", " ")
-     let email = signUpform.email.value
-     let password = signUpform.password.value
-     let confirmPassword = signUpform.confirmPassword.value
 
-     let validate = true
+    setTextErr("#emailErr", "")
+    setTextErr("#passwordErr", "")
+    setTextErr("#confirmPasswordErr", "")
+
+    let email = signUpform.email.value
+    let password = signUpform.password.value
+    let confirmPassword = signUpform.confirmPassword.value
+
+    let validate = true
 
     if(!email){
-         setTextErr("#emailErr", "Email is required")
-         let validate = false
+        setTextErr("#emailErr", "Email is required")
+        validate = false
     }
-     if(!password){
-         setTextErr("#passwordErr", "Password is required")
-         validate = false
-     }
-     if(!confirmPassword){
-         setTextErr("#confirmPasswordErr", "Confirm Password is required")
-         validate = false
-     }
-     if(password.length <6){
-         setTextErr("#passwordErr", "Password must be at least 6 characters")
-         validate = false
-     }else{
-         if(confirmPassword != password){
-             setTextErr("#confirmPasswordErr", "Password does not match")
-             validate = false
-         }
-        }
-        if(validate){
-            let users = [{
-                email: email,
-                password: password
-            }]
-            users.push(user)
-            localStorage.setItem("users", JSON.stringify)
-     }
+    if(!password){
+        setTextErr("#passwordErr", "Password is required")
+        validate = false
+    }
+    if(!confirmPassword){
+        setTextErr("#confirmPasswordErr", "Confirm Password is required")
+        validate = false
     }
 
- let setTextErr = (query, content) =>{
+    if(password.length <6){
+        setTextErr("#passwordErr", "Password must be at least 6 characters") 
+        validate = false 
+    }else{
+        if(confirmPassword != password){
+            setTextErr("#confirmPasswordErr", "Password does not match")
+            validate = false
+        }
+    }
+
+    if(validate){
+        let user = {
+            email: email,
+            password: password
+        }
+        users.push(user)
+        localStorage.setItem("users", JSON.stringify(users))
+    }
+}
+
+let setTextErr = (query, content) =>{
     document.querySelector(query).innerHTML = content
- }
+}
