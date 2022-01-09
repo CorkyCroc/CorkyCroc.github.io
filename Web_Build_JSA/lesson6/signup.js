d4b34f1f89bc3ef916d0ec8b2777917df188cf99
CorkyCroc <86145789+CorkyCroc@users.noreply.github.com>
2022-01-09 12:47:22 +0700

    :)

---
+++ Web_Build_JSA/lesson6/signup.js
@@ -0,0 +1,9 @@
+
+
+let signUnForm = document.getElementById("signInForm")
+signInForm.onsubmit = (e) =>{
+   e.preventDefault()
+    let email = signInForm.email.value
+    let password = signinForm.password.value
+    console.log(email, password);
+}
