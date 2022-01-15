104024a204255f7eebb4b159904021c3874dce04
CorkyCroc <86145789+CorkyCroc@users.noreply.github.com>
2022-01-15 22:36:57 +0700

    log

--- Web_Build_JSA/lesson8/main.js
+++ Web_Build_JSA/lesson8/main.js
@@ -11,23 +11,6 @@ async function Getdata(city) {
     `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=7674da634845e7c2d8c53ab0a48b8e29`
   );
   let data = await drawdata.json();
-
-  let domWeather = document.querySelector("container");
-  let html = `    <div class="container">
-  <p>Hanoi</p>
-  <div class="card">
-    <img src="http://openweathermap.org/img/wn/10d@2x.png" alt="" />
-    <p>25℃</p>
-    <p>Cloudy</p>
-    <form id="input">
-      <label>Enter city:
-        <input name="cityName" type="text" placeholder="City" />
-      </label>
-
-      <button type="submit">submit</button>
-    </form>
-  </div>
-</div>`;
-
-  domWeather.innerHTML = html;
+  console.log(data.main);
+  console.log(data.weather);
 }
--- Web_Build_JSB/lesson8/index.js
+++ Web_Build_JSB/lesson8/index.js
@@ -11,5 +11,15 @@ async function Getdata(city) {
     `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=7674da634845e7c2d8c53ab0a48b8e29`
   );
   let data = await drawdata.json();
-  
+
+
+  let domWeather = document.querySelector(".container");
+  let html = `<div class="card">
+  <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="">
+  <hr>
+  <h1>${Math.floor(data.main.temp-273.15)}°C</h1>
+  <p>${data.weather[0].description}</p>  
+</div>`;
+
+    domWeather.innerHTML = html;
   }
