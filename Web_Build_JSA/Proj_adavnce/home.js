4e6e85187348993064fe9a2730e0a92a946076a2
CorkyCroc <86145789+CorkyCroc@users.noreply.github.com>
2022-02-03 12:31:10 +0700

    finished forecast and embed

---
+++ Web_Build_JSA/Proj_adavnce/home.js
@@ -0,0 +1,23 @@
+let form_submit = document.getElementById("search_form");
+
+form_submit.onsubmit = function (e) {
+  e.preventDefault();
+  let key = form_submit.value;
+
+  getData(key);
+
+  console.log(key);
+
+  form_submit.city.value = "";
+};
+
+let getCity = async function (city) {
+  let Data = await fetch(
+    `https://api.weatherapi.com/v1/forecast.json?key=44ab760595ba41e7a4680133222401&q=Hanoi&days=3&aqi=yes&alerts=yes`
+  );
+
+  console.log(Data);
+
+//   let data = await Data.json();
+
+};
