246a55a781e62ad45032b4ef86b207a1ff0a94af
CorkyCroc <86145789+CorkyCroc@users.noreply.github.com>
2022-02-03 17:22:50 +0700

    link

--- Web_Build_JSA/Proj_adavnce/home.js
+++ Web_Build_JSA/Proj_adavnce/home.js
@@ -4,20 +4,20 @@ form_submit.onsubmit = function (sub) {
     sub.preventDefault();
   let key = form_submit.city.value;
 
-  getData(key);
+//   getData(key);
 
   console.log(key);
 
   form_submit.city.value = "";
 };
 
-// let getCity = async function (city) {
-//   let Data = await fetch(
-//     `https://api.weatherapi.com/v1/forecast.json?key=44ab760595ba41e7a4680133222401&q=Hanoi&days=3&aqi=yes&alerts=yes`
-//   );
+let getCity = async function (city) {
+  let Data = await fetch(
+    `https://api.weatherapi.com/v1/forecast.json?key=44ab760595ba41e7a4680133222401&q=Hanoi&days=3&aqi=yes&alerts=yes`
+  );
 
-//   console.log(Data);
+  let Getdata = await Data.json();
 
-//   let data = await Data.json();
+  console.log(Getdata);
 
-// };
+};
