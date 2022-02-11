1a32b8d91f7fe8078ce20156d21322c8464d6aef
CorkyCroc <86145789+CorkyCroc@users.noreply.github.com>
2022-02-11 14:59:28 +0700

    fix

---
+++ Web_Build_JSA/Proj_adavnce/showData.js
@@ -0,0 +1,24 @@
+let form_submit = document.getElementById("search_form");
+
+form_submit.onsubmit = function (sub) {
+  sub.preventDefault();
+  let key = form_submit.city.value;
+
+//   searchCity(key);
+
+  console.log(key);
+
+  form_submit.city.value = "";
+};
+
+let searchCity = async function (city) {
+  let getData = await fetch(
+    `https://api.weatherapi.com/v1/forecast.json?key=44ab760595ba41e7a4680133222401&q=Hanoi&days=3&aqi=yes&alerts=yes`
+  );
+
+  let data = await getData.json();
+
+  console.log(data.forecast.forecastday.length);
+
+//   for (let i = 0; i < data.forecast.forecastday.length; i++) {}
+};
